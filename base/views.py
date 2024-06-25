3cb39b069894601ce07006f64d8207fda35ff875
Luke <keyanlual@gamil.com>
2024-06-25 17:39:26 +0800

    Use inherate templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+rooms =[
+    {'id':1, 'name':"Room 1"},
+    {'id':2, 'name':"Room 2"},
+    {'id':3, 'name':"Room 3"},
+]
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'home.html',context)
 
 def room(request):
     return render(request, 'room.html')
